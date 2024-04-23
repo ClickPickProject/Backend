bd38ffb66c33aabf7a9538d4c51dbe28e9b70e83
taemintaeminHyeon <taemin14215@gmail.com>
2024-04-23 09:35:29 +0900

    [REFACTOR] 불필요 주석 제거

--- clickpick/src/main/java/com/clickpick/jwt/LoginFilter.java
+++ clickpick/src/main/java/com/clickpick/jwt/LoginFilter.java
@@ -68,8 +68,6 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         String role = auth.getAuthority();
 
-        // String id = jwtUtil.getUsername(token); 아이디 값 추출
-
         String token = jwtUtil.createJwt(username, role, 3600000L); // 3600000ms => 60분
 
         response.addHeader("Authorization", "Bearer " + token);
